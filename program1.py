c59a3b89ab111122f6845fd1d4ac115c895156a8
popcornbrain <tina.elvian@gmail.com>
2016-09-07 13:56:12 +0200

    Dictionaries
    #3

--- program1.py
+++ program1.py
@@ -1,18 +1,4 @@
-all_customers = [22, 32, 52]
-print (all_customers [0])
-print (all_customers [1])
-print (all_customers [2])
+robot = {"price": 900, "count": 2, "tax": 1.25}
+book = {"price": 100, "tax": 1.06}
 
-
-robot_price = 900
-robot_count = 2
-robot_tax = 1.25
-
-book_price = 100
-book_count = 1	
-book_tax = 1.06
-
-print(
-	robot_price * robot_count * robot_tax + book_price * book_count * book_tax
-	)
-
+print(robot["price"] * robot["count"] * robot["tax"] + book["price"] * book["tax"])
